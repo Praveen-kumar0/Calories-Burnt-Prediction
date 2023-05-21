e89954c5db464d432bb4f1a1e4e39be478a87951
Praveen Kumar <114135424+Praveen-kumar0@users.noreply.github.com>
2023-05-21 19:46:42 +0530

    Add files via upload

---
+++ Calories_burnt_prediction_webapp.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 16:05:57 2023
+
+@author: aa
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+# loading the saved model
+loaded_model = pickle.load(open('C:/Users/aa/Desktop/Family/Calories Burnt Prediction/trained_model.sav', 'rb'))
+
+# creating a function for prediction
+def Calories_Burnt_Prediction(input_data):
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    return prediction
+
+
+def main():
+    
+    # giving a title
+    st.title("Calories Burnt Prediction Web App")
+    
+    # getting the input data from the user
+    
+    Gender = st.number_input('Gender ')
+    Age = st.number_input('Age ')
+    Height = st.number_input('Height ')
+    Weight = st.number_input('Weight ')
+    Duration = st.number_input('Duration ')
+    Heart_Rate = st.number_input('Heart_Rate ')
+    Body_Temp = st.number_input('Body_Temp ')
+    
+    
+    # code for prediction
+    result = ''
+    
+    # creating a button for prediction
+    
+    if st.button('Calories Burnt Result'):
+        result = Calories_Burnt_Prediction([Gender, Age, Height, Weight, Duration, Heart_Rate, Body_Temp])
+        
+    st.success(result)
+    
+    
+    
+if __name__ == '__main__':
+    main()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
